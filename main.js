8bba5b0caca3efef9fb6afc1846787e1ed64e5b2
Ana MacBook <anac2828@yahoo.com>
2023-02-19 18:10:09 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-nav');
+const primaryHeader = document.querySelector('.primary-header');
+
+mobileNavToggleBtn.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible')
+    ? mobileNavToggleBtn.setAttribute('aria-expanded', false)
+    : mobileNavToggleBtn.setAttribute('aria-expanded', true);
+  primaryNav.toggleAttribute('data-visible');
+  primaryHeader.toggleAttribute('data-overlay');
+});
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+  adaptiveHeight: false,
+  dots: true,
+  centerMode: true,
+  arrows: false,
+  responsive: {
+    480: {
+      dots: false,
+    },
+  },
+});
